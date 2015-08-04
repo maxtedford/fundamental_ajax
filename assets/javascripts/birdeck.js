8365e3716fca9434f9ce81a3b0be585451043b18
Max Tedford <max.tedford@gmail.com>
2015-08-04 09:58:14 -0600

    formatted the postParams to be a JS object

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
   });
   
   $('#create-post').on('click', function(){
-    var postParams = $('post-description').val();
+    var postParams = { post: { description: $('post-description').val() } };
     console.log(postParams);
   })
 });
