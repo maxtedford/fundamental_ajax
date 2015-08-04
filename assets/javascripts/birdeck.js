98fe76843abf1408417366578cea6cf2bd5fccff
Max Tedford <max.tedford@gmail.com>
2015-08-04 09:26:18 -0600

    change the method to select the div in our html with the id latest-posts

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
     type: 'GET',
     url:  'http://turing-birdie.herokuapp.com/api/v1/posts.json',
     success: function(posts){
-      console.table(posts)
+      $.each(posts, function(){
+        $('#latest-posts')
+      })
     }
   })
 });
