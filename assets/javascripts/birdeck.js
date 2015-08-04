bf717f93e0f3ffeacd78ae6135afddfb0d228a6b
Max Tedford <max.tedford@gmail.com>
2015-08-04 09:34:55 -0600

    add .append method with the paragraph information to pass in--just a Hello for now

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -3,8 +3,10 @@ $(document).ready(function(){
     type: 'GET',
     url:  'http://turing-birdie.herokuapp.com/api/v1/posts.json',
     success: function(posts){
-      $.each(posts, function(){
-        $('#latest-posts')
+      $.each(posts, function(index, post){
+        $('#latest-posts').append(
+          '<p>Hello</p>'
+        )
       })
     }
   })
