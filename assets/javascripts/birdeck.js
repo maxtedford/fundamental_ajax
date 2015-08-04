bd7f69b5b93433377c55ccf062d6f53cd0f812ea
Max Tedford <max.tedford@gmail.com>
2015-08-04 10:33:29 -0600

    add method to fetch posts when fetch posts button is clicked

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -19,6 +19,28 @@ $(document).ready(function(){
     }
   });
   
+  $('#fetch-posts').on('click', function(){
+    $.ajax({
+      type: 'GET',
+      url:  'http://turing-birdie.herokuapp.com/api/v1/posts.json',
+      success: function(posts){
+        $.each(posts, function(index, post){
+          $('#latest-posts').append(
+            "<div class='post' data-id='"
+            + post.id
+            + "'><h6>Published on"
+            + post.created_at
+            + "</h6>"
+            + "<p>"
+            + post.description
+            + "</p>"
+            + "</div>"
+          )
+        })
+      }
+    });
+  });
+  
   $('#create-post').on('click', function(){
     var postParams = { post: { description: $('#post-description').val() } };
     console.log(postParams);
