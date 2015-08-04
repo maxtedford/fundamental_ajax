022621a129a7b31e53cc79231630cc68f148e36c
Max Tedford <max.tedford@gmail.com>
2015-08-04 09:18:05 -0600

    add an action on success which throws all posts to the console

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -1,6 +1,9 @@
-$(document).ready(function (){
+$(document).ready(function(){
   $.ajax({
     type: 'GET',
-    url:  'http://turing-birdie.herokuapp.com/api/v1/posts.json'
+    url:  'http://turing-birdie.herokuapp.com/api/v1/posts.json',
+    success: function(posts){
+      console.table(posts)
+    }
   })
 });
